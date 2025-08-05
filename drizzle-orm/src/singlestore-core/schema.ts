4db8ed4d5c0a911e90063c7f70f61d91a04d7a0c
James Arnott <james@rno.tt>
2025-08-05 15:22:19 +0000

    chore: revert changes

--- drizzle-orm/src/singlestore-core/schema.ts
+++ drizzle-orm/src/singlestore-core/schema.ts
@@ -1,4 +1,4 @@
-import { entityKind } from '~/entity.ts';
+import { entityKind, is } from '~/entity.ts';
 import { type SingleStoreTableFn, singlestoreTableWithSchema } from './table.ts';
 /* import { type singlestoreView, singlestoreViewWithSchema } from './view.ts'; */
 
@@ -12,12 +12,29 @@ export class SingleStoreSchema<TName extends string = string> {
 	table: SingleStoreTableFn<TName> = (name, columns, extraConfig) => {
 		return singlestoreTableWithSchema(name, columns, extraConfig, this.schemaName);
 	};
-
-	/* view: SingleStoreViewFn<TName> = (name, columns, view) => {
-		return singlestoreViewWithSchema(name, columns, view, this.schemaName);
-	}; */
+	/*
+	view = ((name, columns) => {
+		return singlestoreViewWithSchema(name, columns, this.schemaName);
+	}) as typeof singlestoreView; */
 }
 
-export function singlestoreDatabase<TName extends string>(name: TName): SingleStoreSchema<TName> {
+/** @deprecated - use `instanceof SingleStoreSchema` */
+export function isSingleStoreSchema(obj: unknown): obj is SingleStoreSchema {
+	return is(obj, SingleStoreSchema);
+}
+
+/**
+ * Create a SingleStore schema.
+ * https://docs.singlestore.com/cloud/create-a-database/
+ *
+ * @param name singlestore use schema name
+ * @returns SingleStore schema
+ */
+export function singlestoreDatabase<TName extends string>(name: TName) {
 	return new SingleStoreSchema(name);
 }
+
+/**
+ * @see singlestoreDatabase
+ */
+export const singlestoreSchema = singlestoreDatabase;
